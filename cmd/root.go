4faa35ba030a2a8778484be79b232e6c8feec5dd
Riley Flynn <riley@rileyflynn.me>
2023-09-09 22:43:42 -0230

    Actual functionality

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"errors"
 	"os"
 
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -14,7 +17,66 @@ var rootCmd = &cobra.Command{
 	Use:  "command-clearer",
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Info().Msg("TODO")
+		err := godotenv.Load()
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			log.Fatal().Err(err).Msg("Failed to load .env file")
+		}
+
+		var discordToken string
+		var discordAppId string
+
+		discordTokenVar, _ := cmd.Flags().GetString("discord-token-var")
+		if discordTokenVar != "" {
+			discordToken = os.Getenv(discordTokenVar)
+		} else {
+			discordToken, _ = cmd.Flags().GetString("discord-token")
+		}
+
+		discordAppIdVar, _ := cmd.Flags().GetString("discord-app-id-var")
+		if discordAppIdVar != "" {
+			discordAppId = os.Getenv(discordAppIdVar)
+		} else {
+			discordAppId, _ = cmd.Flags().GetString("discord-app-id")
+		}
+
+		if discordToken == "" {
+			log.Fatal().Msg("Discord token is required - specify --discord-token or --discord-token-var")
+		}
+
+		if discordAppId == "" {
+			log.Fatal().Msg("Discord application ID is required - specify --discord-app-id or --discord-app-id-var")
+		}
+
+		session, err := discordgo.New("Bot " + discordToken)
+		checkError(err, "Failed to create Discord session")
+
+		var deletedCommandCount int
+
+		// TODO: better progress reporting
+		globalCommands, err := session.ApplicationCommands(discordAppId, "")
+		checkError(err, "Failed to get global commands")
+
+		for _, command := range globalCommands {
+			err = session.ApplicationCommandDelete(discordAppId, "", command.ID)
+			checkError(err, "Failed to delete command")
+			deletedCommandCount++
+		}
+
+		guilds, err := session.UserGuilds(100, "", "")
+		checkError(err, "Failed to get guilds")
+
+		for _, guild := range guilds {
+			guildCommands, err := session.ApplicationCommands(discordAppId, guild.ID)
+			checkError(err, "Failed to get guild commands")
+
+			for _, guildCommand := range guildCommands {
+				err = session.ApplicationCommandDelete(discordAppId, guildCommand.GuildID, guildCommand.ID)
+				checkError(err, "Failed to delete guild command")
+				deletedCommandCount++
+			}
+		}
+
+		log.Info().Msgf("Deleted %d commands successfully!", deletedCommandCount)
 	},
 }
 
@@ -22,6 +84,10 @@ func init() {
 	cobra.OnInitialize(initLogging)
 
 	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "log level")
+	rootCmd.Flags().String("discord-token-var", "", "name of the environment variable containing the Discord token")
+	rootCmd.Flags().String("discord-token", "", "Discord token")
+	rootCmd.Flags().String("discord-app-id-var", "", "name of the environment variable containing the Discord application ID")
+	rootCmd.Flags().String("discord-app-id", "", "Discord application ID")
 }
 
 func initLogging() {
--- go.mod
+++ go.mod
@@ -3,14 +3,18 @@ module github.com/nint8835/command-clearer
 go 1.21.0
 
 require (
+	github.com/bwmarrin/discordgo v0.27.1
+	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.30.0
 	github.com/spf13/cobra v1.7.0
 )
 
 require (
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.12.0 // indirect
 )
